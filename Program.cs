db2a55df4a9f9dd636aac098440dffe91dff3532
DESKTOP-V0H394L\User <sebastianjogosextra@gmail.com>
2022-08-04 19:18:26 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("\nO que é, o que é?");
+Thread.Sleep(1250);
+Console.WriteLine("Um ponto vermelho no topo de um castelo?");
+Thread.Sleep(5000);
+Console.WriteLine("\nÉ a pimenta do reino!\n");
